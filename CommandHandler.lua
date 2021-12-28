7749e7e5adfaf3f2fa520126df31abda7031cbb5
GFXTI <65578807+GFXTI@users.noreply.github.com>
2021-12-28 01:50:46 -0800

    Update CommandHandler.lua

--- CommandHandler.lua
+++ CommandHandler.lua
@@ -15,6 +15,7 @@
 -- Variables
 
 local CH = {}
+CH.GivenAdmin = {}
 CH.Configuration = {}
 CH.Commands = {}
 CH.Configuration.Prefix = "-"
@@ -22,6 +23,8 @@ CH.Configuration.Split = "|" -- if you ever use multiple commands
 
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
+
+table.insert(CH.GivenAdmin,LocalPlayer)
 
 -- Command Handler functions (important)
 
@@ -50,7 +53,7 @@ function CH:ChangeConfiguration(Setting,ChangeTo)
     CH.Configuration[Setting] = ChangeTo
 end
 
-function CH:AddCommand(Command, Aliases, Info, Function) -- if you like functions better
+function CH:AddCommand(Command, Aliases, Info, Function, IsUsableByOthers) -- if you like functions better
     assert(type(Command) == "string", "1st Argument to AddCommand is not a string")
     assert(type(Aliases) == "table", "2nd Argument to AddCommand is not a table")
     assert(type(Info) == "string", "3rd Argument to AddCommand is not a string")
@@ -62,6 +65,7 @@ function CH:AddCommand(Command, Aliases, Info, Function) -- if you like function
     Final["Aliases"] = Aliases
     Final["Info"] = Info
     Final["Function"] = Function
+    Final["IsUsableByOthers"] = IsUsableByOthers or false
     
     table.insert(CH.Commands,Final)    
 end
@@ -112,34 +116,65 @@ function CH:RemoveCommand(ToRemove)
     end
 end
 
--- chatted
-
-LocalPlayer.Chatted:Connect(function(msg)
-    local commandamount = msg:split(tostring(CH.Configuration.Split))
-    local prefix = tostring(CH.Configuration.Prefix)
-    
-    if commandamount[1]:sub(1,#prefix) == prefix then
-        for i = 1,#commandamount do
-            local firstarg
-            
-            if i == 1 then -- fix to sum funtcion dieding because prefix
-                local prestring = commandamount[i]:split(" ")[1]
-                firstarg = prestring:lower():sub(2,#prestring)
-            else
-                firstarg = commandamount[i]:split(" ")[1]:lower()
+function CH:HandleMessage(msg,plr)
+    if table.find(CH.GivenAdmin,plr) then
+        local commandamount = msg:split(tostring(CH.Configuration.Split))
+        local prefix = tostring(CH.Configuration.Prefix)
+        
+        if commandamount[1]:sub(1,#prefix) == prefix then
+            for i = 1,#commandamount do
+                local firstarg
+                
+                if i == 1 then -- fix to sum funtcion dieding because prefix
+                    local prestring = commandamount[i]:split(" ")[1]
+                    firstarg = prestring:lower():sub(2,#prestring)
+                else
+                    firstarg = commandamount[i]:split(" ")[1]:lower()
+                end
+                
+                local args = commandamount[i]:split(" ")
+                local found, cmdtable = CH:FindCommand(firstarg)
+                table.remove(args,1)
+                
+                if found and plr == LocalPlayer or cmdtable.IsUsableByOthers then
+                    task.spawn(function()cmdtable["Function"](args)end)
+                end
+                
             end
-            
-            local args = commandamount[i]:split(" ")
-            local found, cmdtable = CH:FindCommand(firstarg)
-            table.remove(args,1)
-            
-            if found then
-                task.spawn(function()cmdtable["Function"](args)end)
-            end
-            
         end
     end
+end
 
+function CH:ChangeAdminPerms(plr)
+    local function check()
+        for i,v in pairs(CH.GivenAdmin) do
+            if v == plr then
+                return true, i
+            end
+        end
+    end
+    
+    local c,p = check()
+    
+    if not c then
+        table.insert(CH.GivenAdmin,plr)
+    else
+        table.remove(CH.GivenAdmin,p)
+    end
+end
+
+-- chatted
+
+for i,v in pairs(Players:GetPlayers()) do 
+    v.Chatted:Connect(function(msg)
+        CH:HandleMessage(msg,v)
+    end)
+end
+
+Players.PlayerAdded:Connect(function(v)
+    v.Chatted:Connect(function(msg)
+        CH:HandleMessage(msg,v)
+    end)
 end)
 
 return CH
